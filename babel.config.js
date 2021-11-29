15f7ff1e707d8de3aa8e8a8aa64fca01c6c11709
michiharu <michi1diver@gmail.com>
2021-11-29 23:48:20 +0900

    :shirt: create src dir and move files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+          alias: { '~': './src' },
+        },
+      ],
+    ],
   };
 };
